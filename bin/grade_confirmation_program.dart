3c9f461e2550011cfd2282bae46cbce3ffd3f294
지은성 <ddrr2852@gmail.com>
2025-12-12 12:50:11 +0900

    feat : "grade_confirmation_program" 필수 문제 주석 처리 후 도전문제 메인을 연결해놓음

--- bin/grade_confirmation_program.dart
+++ bin/grade_confirmation_program.dart
@@ -1,11 +1,20 @@
 // 메인 실행 파일
 
-import 'package:grade_confirmation_program/score_check.dart';
+import 'package:grade_confirmation_program/challenge/challenge_excellent_output.dart';
+// 도전문제
 
+// import 'package:grade_confirmation_program/score_check.dart';
+// 필수문제
+
+// 필수문제
+// void main() {
+//   //
+//   scoreCheck();
+//   // 실행 처음부분을 가져오면 됨
+// }
+
+// 도전문제
 void main() {
   //
-  scoreCheck(); 
-  // 실행 처음부분을 가져오면 됨
+  selectMenu();
 }
-
-// 도전 과제할때 필요 없는거 주석처리해서 같은 폴더인 dart_assignment2 에서 시도해보기
